08fa185681c31e643e520009303574bd379acec7
Ryan McCue <me@ryanmccue.info>
2016-11-25 16:40:28 +1000

    Use the correct package for the PHP version in use

--- modules/memcache/manifests/init.pp
+++ modules/memcache/manifests/init.pp
@@ -1,14 +1,22 @@
 class memcache (
-	$path = "/vagrant/extensions/memcache"
+	$path = "/vagrant/extensions/memcache",
+	$php_version
 ) {
 	package { 'memcached':
 		ensure => latest
 	}
-	
-	package { 'php5-memcache':
+
+	if versioncmp( "${php_version}", '5.4') <= 0 {
+		$php_package = 'php5'
+	}
+	else {
+		$php_package = "php${php_version}"
+	}
+
+	package { "${php_package}-memcache":
 		ensure  => latest,
 		require => Package['memcached'],
-		notify  => Service['php5-fpm']
+		notify  => Service["${php_package}-fpm"]
 	}
 
 	file { "${path}/local-config.php":
