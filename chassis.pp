793fb9f652481a0a9f78970613d3281bd5952959
Matthew Haines-Young <matthew@matth.eu>
2016-12-08 22:54:39 +0000

    Remove old phpversion var

--- chassis.pp
+++ chassis.pp
@@ -1,6 +1,5 @@
 $memcache_config = sz_load_config()
 
 class { 'memcache':
-	path => '/vagrant/extensions/memcache',
-	php_version => $memcache_config[php]
+	path => '/vagrant/extensions/memcache'
 }
