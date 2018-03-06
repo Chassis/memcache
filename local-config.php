7b59c8c5f950e514b66dca32d8078c77839ebe8b
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-06 16:38:01 +1000

    Fix puppet linting

---
+++ local-config.php
@@ -0,0 +1,5 @@
+<?php
+
+// This is generated automatically by Puppet. Edit at your own risk.
+
+$memcached_servers = array( '127.0.0.1:11211' );
--- modules/memcache/manifests/init.pp
+++ modules/memcache/manifests/init.pp
@@ -1,23 +1,22 @@
 # A Chassis extension to install and configure memcached on your server.
 class memcache (
-  $path = '/vagrant/extensions/memcache',
-  $memcache_config = sz_load_config()
+	$path = '/vagrant/extensions/memcache',
+	$memcache_config = sz_load_config()
 ) {
-  package { 'memcached':
-    ensure => latest
-  }
+	package { 'memcached':
+		ensure => latest
+	}
 
-  if versioncmp( "${memcache_config[php]}", '5.4') <= 0 {
-    $php_package = 'php5'
-  }
-  else {
-    $short_ver = regsubst("${memcache_config[php]}", '^(\d+\.\d+)\.\d+$', '\1')
-    $php_package = "php${short_ver}"
-  }
+	if versioncmp( $memcache_config[php], '5.4') <= 0 {
+		$php_package = 'php5'
+	} else {
+		$short_ver = regsubst($memcache_config[php], '^(\d+\.\d+)\.\d+$', '\1')
+		$php_package = "php${short_ver}"
+	}
 
-  package { ["${php_package}-memcache", "${php_package}-memcached"]:
-    ensure  => latest,
-    require => Package['memcached'],
-    notify  => Service["${php_package}-fpm"]
-  }
+	package { ["${php_package}-memcache", "${php_package}-memcached"]:
+		ensure  => latest,
+		require => Package['memcached'],
+		notify  => Service["${php_package}-fpm"]
+	}
 }
